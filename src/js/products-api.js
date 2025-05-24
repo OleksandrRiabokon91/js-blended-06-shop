c9a43340254785268a3718f830a99bc948cc98cc
Oleksandr Riabokon <riabokon.oleksandr@gmail.com>
2025-05-24 15:21:40 +0300

    add first task

--- src/js/products-api.js
+++ src/js/products-api.js
@@ -1,1 +1,11 @@
 // Функції для роботи з бекендом
+// запит до ендпоінта
+import axios from 'axios';
+export async function getCategory(query) {
+  const baseURL = 'https://dummyjson.com/products';
+  const endPoint = '/category-list';
+  const url = baseURL + endPoint;
+
+  const res = await axios.get(url);
+  return res.data;
+}
--- src/js/render-function.js
+++ src/js/render-function.js
@@ -1,1 +1,13 @@
 //Функцію для створення, рендеру або видалення розмітки
+
+function crieitElem(string) {
+  const markap = `<li class="categories__item">
+   <button class="categories__btn" type="button">${string}</button>
+ </li>`;
+  return markap;
+}
+
+export function crieitElements(params) {
+  params.unshift('All');
+  return params.map(crieitElem).join('');
+}
